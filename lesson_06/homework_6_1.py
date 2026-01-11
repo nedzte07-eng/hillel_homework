38b1573f58c58c8d375f51b7fcbe03720c9ba37b
nedzte07-eng <nedzte07@gmail.com>
2026-01-11 12:13:00 +0200

    fix1 for 6.1

--- lesson_06/homework_6_1.py
+++ lesson_06/homework_6_1.py
@@ -1,5 +1,5 @@
 string = input("Enter the string: ")
-list_of_elements = string.split()
+list_of_elements = list(string)
 count = 0
 for element in list_of_elements:
   if list_of_elements.count(element) == 1:
