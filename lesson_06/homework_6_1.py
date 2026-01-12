f2dd608c6f050b07c479d05ccc91e9acfda66feb
nedzte07-eng <nedzte07@gmail.com>
2026-01-12 09:31:18 +0200

    fix2 for 6.1

--- lesson_06/homework_6_1.py
+++ lesson_06/homework_6_1.py
@@ -1,9 +1,8 @@
 string = input("Enter the string: ")
 list_of_elements = list(string)
-count = 0
-for element in list_of_elements:
-  if list_of_elements.count(element) == 1:
-      count = count + 1
+set_of_elements = set(list_of_elements)
+print(set_of_elements)
+count = sum(1 for element in set_of_elements if list_of_elements.count(element) == 1)
 
 if count > 10:
     print(True)
