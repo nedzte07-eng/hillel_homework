9769c301d9f00fcc17372e1e3c454b8773160a28
nedzte07-eng <nedzte07@gmail.com>
2026-02-01 15:14:17 +0200

    homework_11 first try

---
+++ lesson_11/lesson_11.py
@@ -0,0 +1,24 @@
+example = ["1,2,3,4", "1,2,   3,4, 50   ", "qwerty1,2,3", "0,  0,0  ,0", "-5"]
+
+def totals_of_iists_items (data_list):
+    result = []
+    for i in example:
+
+        i = i.strip()
+        splited_list = i.split(',')
+        total = 0
+        for j in splited_list:
+            try:
+                j = j.strip()
+                total += int(j)
+            except ValueError as e:
+                print(f"Неправильні дані, виникла помилка {e}")
+                total = 'failure'
+                break
+        if total == 'failure':
+            continue
+        result.append(total)
+    return result
+
+print(totals_of_iists_items(example))
+
