eb85dfd5ff2fb6cd291f885e02df9b9badca72d0
nedzte07-eng <nedzte07@gmail.com>
2026-02-09 10:42:52 +0200

    03 try of homework_13

--- tests/api_tests/lesson_13_tests/test_homework_13.py
+++ tests/api_tests/lesson_13_tests/test_homework_13.py
@@ -1,6 +1,6 @@
 # 1)
 import logging
-
+import pytest
 import requests
 
 _log = logging.getLogger('Main')
@@ -22,57 +22,59 @@ USER_CREDENTIALS = {
 CAR_CREDENTIALS = {
   "carBrandId": 2,
   "carModelId": 8,
-  "mileage": 444
+  "mileage": 555
 }
+
+@pytest.fixture
+def session_cookie():
+    response = requests.post(
+        f"{BASE_URL}api/auth/signin",
+        auth=AUTH,
+        json=USER_CREDENTIALS
+    )
+    assert response.status_code == 200, "Login failed"
+    return response.cookies.get("sid")
+
+@pytest.fixture
+def car_id(session_cookie):
+    response_car = requests.get(
+        f"{BASE_URL}api/cars", headers={"Cookie": f"sid={session_cookie}"})
+    data = response_car.json()["data"]
+    assert data, "Cars were not found"
+    return data[0]["id"]
+
 
 class TestContent:
 
-    def test_signin_with_existence_user(self):
-        print('Sign in with existence user')
-        response = requests.post(f'{BASE_URL}api/auth/signin', auth=AUTH, json=USER_CREDENTIALS)
-        assert response.status_code == 200, "Content was not created"
+    def test_signin_with_existence_user(self, session_cookie):
+        assert session_cookie is not None
 
-    def test_adding_a_car(self):
-        print('Adding a car')
-        response = requests.post(f'{BASE_URL}api/auth/signin', auth=AUTH, json=USER_CREDENTIALS)
-        assert response.status_code == 200, "Content was not created"
-        token_session = response.cookies.get("sid")
-        response_car = requests.post(f'{BASE_URL}api/cars', auth=AUTH, json=CAR_CREDENTIALS, headers={"Cookie": f"sid={token_session}"})
-        assert response_car.status_code == 201, "Content was not created"
+    def test_adding_a_car(self, session_cookie):
+        response_car = requests.post(
+            f"{BASE_URL}api/cars",
+            auth=AUTH,
+            json=CAR_CREDENTIALS,
+            headers={"Cookie": f"sid={session_cookie}"}
+        )
+        assert response_car.status_code == 201, "Car was not created"
+
+    def test_if_car_is_present(self, session_cookie, car_id):
+        print(car_id)
+        assert car_id != [], "Cars were not found"
+
+    def test_deleting_a_car(self, session_cookie, car_id):
+        response_car = requests.delete(f"{BASE_URL}api/cars/{car_id}", headers={"Cookie": f"sid={session_cookie}"})
+        assert response_car.status_code == 200, "Car was not deleted"
+
+    def test_if_car_is_absent(self, session_cookie):
+        response_car = requests.get(
+            f"{BASE_URL}api/cars", headers={"Cookie": f"sid={session_cookie}"})
+        data = response_car.json()["data"]
+        assert data == [], "Cars were found"
+
+    def test_user_logout(self, session_cookie):
+        response = requests.get(f"{BASE_URL}api/auth/logout", headers={"Cookie": f"sid={session_cookie}"})
+        assert response.status_code == 200, "Logout failed"
 
 
-        # yield response , 123, [1, 34]
-        # #POST_TEST
-        # print('Deleting content...')
-        # response = requests.delete(f'{base_url}/content/{response.json().get('id')}')
-        # assert response.status_code == 200, "Unable delete content"
 
-
-    # def add_car(self, signin_with_existence_user):
-    #     _log.info('Adding a car')
-    #
-
-
-
-
-    """
-    # 2)
-    @pytest.mark.positive
-    def auth_login_negative_401():
-        content = {'username': '12312312', 'password': '123123123'}
-        response = requests.post(f'{BASE_URL}', json=content)
-        assert response.status_code == 401, "Content was not created"
-    
-    # 3) створіть conftest file  і зробіть в ньому фікстуру -> Створіть окремий файл /users
-    
-    
-    def test_get_content(auth_login):
-        # assert False
-        response, headers = auth_login
-        print('Getting content...')
-        # headers = headers -> очікуєтся, що фікстура зробить логін зі статусром 200 і в ньому буде лежати ТОКЕН
-        response_get = requests.get(f'{base_url}/users', headers=headers) #url 'http://127.0.0.1:7070/api/v1/users'
-        assert response_get.status_code == 200, "Unable to get content"
-        server_content = response_get.json().get('content')
-    
-    """
