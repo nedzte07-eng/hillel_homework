389cec0d48609762390ee6273f349d08bcf141ae
nedzte07-eng <nedzte07@gmail.com>
2026-02-19 10:06:16 +0200

    02 Homework_16, csv function is added

--- lessons/Lesson_16/homework_16.py
+++ lessons/Lesson_16/homework_16.py
@@ -1,8 +1,9 @@
+import csv
+
 import xmltodict
 
-from constant import INITIAL_DATA_XMLS_LESSON_16
+from constant import INITIAL_DATA_XMLS_LESSON_16, INITIAL_DATA_CSVS_LESSON_16, PROCESSED_DATA_CSVS_LESSON_16
 import logging
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -15,11 +16,10 @@ console_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 
-with open(INITIAL_DATA_XMLS_LESSON_16/'groups.xml', mode='r') as file:
-    logger.info(f"Path to XMLs: {INITIAL_DATA_XMLS_LESSON_16}")
+with open(INITIAL_DATA_XMLS_LESSON_16 / 'groups.xml', mode='r') as file:
+    # logger.info(f"Path to XMLs: {INITIAL_DATA_XMLS_LESSON_16}")
     data_xml = file.read()
     data_xml_parse = xmltodict.parse(data_xml)
-    print(data_xml_parse)
     incoming_values = []
     for group in data_xml_parse['groups']['group']:
         timing = group.get('timingExbytes')
@@ -28,5 +28,22 @@ with open(INITIAL_DATA_XMLS_LESSON_16/'groups.xml', mode='r') as file:
 
     logger.info(f"timingExbytes/incoming: {incoming_values}")
 
-# print(data_xml)
-# print(data_xml_parse)
+
+def csv_unduplicates(filename = 'r-m-c.csv'):
+    unduplicates_csv = []
+    with open(INITIAL_DATA_CSVS_LESSON_16 / filename, mode='r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            if row not in unduplicates_csv:
+                unduplicates_csv.append(row)
+
+    headers = unduplicates_csv[0]
+    payload = unduplicates_csv[1:]
+    with open(PROCESSED_DATA_CSVS_LESSON_16 / filename, mode='w', newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+        for row in payload:
+            writer.writerow(row)
+
+csv_unduplicates()
+csv_unduplicates('random-michaels.csv')
