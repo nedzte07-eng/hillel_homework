f7683ad4ccc1b02d4706e94ba60df6ba858d24e0
nedzte07-eng <nedzte07@gmail.com>
2026-02-20 17:42:11 +0200

    01 some fixes

--- lessons/Lesson_16/homework_16.py
+++ lessons/Lesson_16/homework_16.py
@@ -8,73 +8,77 @@ import json
 from constant import INITIAL_DATA_XMLS_LESSON_16, INITIAL_DATA_CSVS_LESSON_16, INITIAL_DATA_JSONS_LESSON_16, \
     PROCESSED_DATA_CSVS_LESSON_16, PROCESSED_DATA_JSONS_LESSON_16
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
 with open(INITIAL_DATA_XMLS_LESSON_16 / 'groups.xml', mode='r') as file:
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-
-    logger.addHandler(console_handler)
-    # logger.info(f"Path to XMLs: {INITIAL_DATA_XMLS_LESSON_16}")
     data_xml = file.read()
-    data_xml_parse = xmltodict.parse(data_xml)
-    incoming_values = []
-    for group in data_xml_parse['groups']['group']:
-        timing = group.get('timingExbytes')
-        if timing and 'incoming' in timing:
-            incoming_values.append(timing['incoming'])
 
-    logger.info(f"timingExbytes/incoming: {incoming_values}")
+data_xml_parse = xmltodict.parse(data_xml)
+incoming_values = []
+for group in data_xml_parse['groups']['group']:
+    timing = group.get('timingExbytes')
+    if timing and 'incoming' in timing:
+        incoming_values.append(timing['incoming'])
+
+logger.info(f"timingExbytes/incoming: {incoming_values}")
 
 
-def csv_unduplicates(filename='r-m-c.csv'):
-    unduplicates_csv = []
-    with open(INITIAL_DATA_CSVS_LESSON_16 / filename, mode='r') as file:
-        csv_reader = csv.reader(file)
-        for row in csv_reader:
-            if row not in unduplicates_csv:
-                unduplicates_csv.append(row)
+# def csv_unduplicates(filename='r-m-c.csv'):
+#     unduplicates_csv = []
+#     with open(INITIAL_DATA_CSVS_LESSON_16 / filename, mode='r') as file:
+#         csv_reader = csv.reader(file)
+#         for row in csv_reader:
+#             if row not in unduplicates_csv:
+#                 unduplicates_csv.append(row)
+#
+#     headers = unduplicates_csv[0]
+#     payload = unduplicates_csv[1:]
+#     with open(PROCESSED_DATA_CSVS_LESSON_16 / f'nedzelnytskyi_{filename}', mode='w', newline="") as file:
+#         writer = csv.writer(file)
+#         writer.writerow(headers)
+#         for row in payload:
+#             writer.writerow(row)
 
-    headers = unduplicates_csv[0]
-    payload = unduplicates_csv[1:]
-    with open(PROCESSED_DATA_CSVS_LESSON_16 / f'nedzelnytskyi_{filename}', mode='w', newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(headers)
-        for row in payload:
-            writer.writerow(row)
 
 def csv_unduplicates_use_set(filename='random-michaels.csv'):
-
     with open(INITIAL_DATA_CSVS_LESSON_16 / filename, mode='r') as f:
         csv_reader = csv.reader(f)
-        headers = next(csv_reader)
-        rows = [tuple(row) for row in csv_reader]
+        # headers = next(csv_reader)
+        # rows = [tuple(row) for row in csv_reader]
+        seen = set()
+        payload = []
 
-    payload = list(set(rows))
+        for row in csv_reader:
+            row_tuple = tuple(row)
 
+            if row_tuple not in seen:
+                seen.add(row_tuple)
+                payload.append(row)
 
-    with open(PROCESSED_DATA_CSVS_LESSON_16 / f'nedzelnytskyi_{filename}', mode='w', newline="", encoding='utf-8') as file:
+    with open(PROCESSED_DATA_CSVS_LESSON_16 / f'nedzelnytskyi_{filename}', mode='w', newline="",
+              encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(headers)
         writer.writerows(payload)
 
 
-
-csv_unduplicates('r-m-c.csv')
+csv_unduplicates_use_set('r-m-c.csv')
 csv_unduplicates_use_set('random-michaels.csv')
 
+logging.basicConfig(
+    filename=PROCESSED_DATA_JSONS_LESSON_16 / "json_nedzelnytskyi.log",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 def is_valid_json(file_json: str) -> bool:
-    logging.basicConfig(
-        filename=PROCESSED_DATA_JSONS_LESSON_16 / "json_nedzelnytskyi.log",
-        level=logging.ERROR,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-
     try:
         with open(INITIAL_DATA_JSONS_LESSON_16 / file_json, "r", encoding="utf-8") as f:
             json.load(f)
