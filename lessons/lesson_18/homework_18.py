a2854bdb05306f3499f3873eea477674e90c1c7f
nedzte07-eng <nedzte07@gmail.com>
2026-02-25 19:09:13 +0200

    decorators are added

--- lessons/lesson_18/homework_18.py
+++ lessons/lesson_18/homework_18.py
@@ -1,4 +1,8 @@
 # Напишіть генератор, який повертає послідовність парних чисел від 0 до N.
+import logging
+import inspect
+
+
 def even_numbers_generator(n):
     a = 0
     while a <= n:
@@ -49,24 +53,29 @@ class ReversedListIterator:
         except:
             raise StopIteration
 
-rev_list = ReversedListIterator([1,2,3,4,5,6,7,8,9,10])
+
+rev_list = ReversedListIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 for i in rev_list:
     print(i)
 print("-" * 80)
 
-#Напишіть ітератор, який повертає всі парні числа в діапазоні від 0 до N.
+
+# Напишіть ітератор, який повертає всі парні числа в діапазоні від 0 до N.
 class EvenNumbersIterator:
     def __init__(self, n):
         self.n = n
         self.num = -2
+
     def __iter__(self):
         return self
+
     def __next__(self):
         self.num += 2
         if self.num > self.n:
             raise StopIteration
         return self.num
+
 
 ev_nums = EvenNumbersIterator(15)
 
@@ -74,15 +83,56 @@ for i in ev_nums:
     print(i)
 print("-" * 80)
 
+# Напишіть декоратор, який логує аргументи та результати викликаної функції.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
 
 
+def decorator_arguments_results(func):
+    def wrapper(*args):
+        # print(f"Arguments are / is {args}")
+        logger.info(f"Arguments are / is {args}")
+        result = func(*args)
+        # print(f"Result is {result}")
+        logger.info(f"Result is {result}")
+
+    return wrapper
 
 
+@decorator_arguments_results
+def max_of_list(n: list) -> int:
+    return max(n)
 
 
+maximum = max_of_list([1, 6, 4])
 
 
+# Створіть декоратор, який перехоплює та обробляє винятки, які виникають в ході виконання функції.
+def decorator_of_exceptions(func):
+    def wrapper(*args):
+        try:
+            func(*args)
+        except Exception as e:
+            logger.error(f"The function is failed with {e} exception")
+
+    return wrapper
 
 
+@decorator_of_exceptions
+def positive_value(n: int) -> int:
+    if n > 0:
+        return n
+    else:
+        raise TypeError("VALUE IS NEGATIVE")
 
 
+positive = positive_value(10)
+negative = positive_value(-10)
