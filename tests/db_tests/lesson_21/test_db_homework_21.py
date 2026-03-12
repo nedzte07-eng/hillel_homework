7db2bdc46ef6d89377798780e9e467ff0eaea505
nedzte07-eng <nedzte07@gmail.com>
2026-03-12 11:33:28 +0200

    homework 21 commit 04

--- tests/db_tests/lesson_21/test_db_homework_21.py
+++ tests/db_tests/lesson_21/test_db_homework_21.py
@@ -1,12 +1,22 @@
-def test_add_dog(cursor_con):
+# def test_add_dog(cursor_con):
+#
+#     cursor, conn = cursor_con
+#     cursor.execute('''INSERT INTO public.dogs ("name", breed) VALUES ('Nora', 'Cane Corso') returning id''')
+#     dog_id = cursor.fetchone()[0]
+#
+#     cursor.execute(f'''select "name" from public.dogs where id = {dog_id}''')
+#     name_ar = cursor.fetchone()[0]
+#
+#     assert 'Nora' == name_ar
+#     conn.commit()
 
-    cursor, conn = cursor_con
-    cursor.execute('''INSERT INTO public.dogs ("name", breed) VALUES ('Nora', 'Cane Corso') returning id''')
-    dog_id = cursor.fetchone()[0]
+def test_join_hw21(cursor):
+    cursor = cursor
+    cursor.execute('''select "name", category_name from public.products join public.categories on public.products.category = public.categories.category_id''')
+    rows = cursor.fetchall()
 
-    cursor.execute(f'''select "name" from public.dogs where id = {dog_id}''')
-    name_ar = cursor.fetchone()[0]
+    print('\n')
 
-    assert 'Nora' == name_ar
-    conn.commit()
+    for row in rows:
+        print(row)
 
