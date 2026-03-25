4b3e354cdc9299c29f9e6780a1d4818a9d3a871c
nedzte07-eng <nedzte07@gmail.com>
2026-03-25 18:51:58 +0200

    Xpath added

---
+++ lessons/lesson_25/homework_25.py
@@ -0,0 +1,74 @@
+
+#XPath
+
+#Header
+#Home Button
+"""//a[text()='Home']"""
+#About Button
+"""//button[text()='About']"""
+#Contacts Button
+"""//button[text()='Contacts']"""
+#Guest log in Button
+"""//button[text()='Guest log in']"""
+#Sighn In Button
+"""//button[text()='Sign In']"""
+
+
+
+#Do More text
+"""//h1[text()='Do more!']"""
+#or
+"""//div[@class='hero-descriptor']/h1[1]"""
+
+#With the help of the Hillel ... text
+"""//div[@class='hero-descriptor']/p[1]"""
+
+#Sign Up Buton
+"""//div[@class='hero-descriptor']/button[1]"""
+#or
+"""//button[text()='Sign up']"""
+
+#Player with video
+"""//div[@class='player-controls-content']"""
+
+#Fuel expenses image
+"""(//div[@class='about-block'])[1]/div/img"""
+#or
+"""(//img[@class='about-picture_img'])[1]"""
+
+
+#Log fuel expenses text
+"""(//div[@class='about-block'])[1]/p[1]"""
+
+#Keep track of your ... text
+"""(//div[@class='about-block'])[1]/p[2]"""
+#or
+"""(//p[@class='about-block_descr lead'])[1]"""
+
+#Instructions image
+"""(//div[@class='about-block'])[2]/div/img"""
+#or
+"""(//img[@class='about-picture_img'])[2]"""
+
+#Instructions and manuals text
+"""(//div[@class='about-block'])[2]/p[1]"""
+
+#Watch over 100 ... text
+"""(//div[@class='about-block'])[2]/p[2]"""
+#or
+"""(//p[@class='about-block_descr lead'])[2]"""
+
+#Telegram icon
+"""//span[@class='socials_icon icon icon-telegram']"""
+
+#linl to Hillel page
+"""//a[@href='https://ithillel.ua']"""
+
+#© 2021 Hillel IT school text
+"""//p[text()='© 2021 Hillel IT school']"""
+
+#link to support email
+"""//a[@class='contacts_link h4']"""
+
+
+
