5ee8b3a9d117ce0fcdd42c7cf3d8e69ad3546931
nedzte07-eng <nedzte07@gmail.com>
2026-03-25 21:15:57 +0200

    Some CSS locators added 01

--- lessons/lesson_25/homework_25_xpath.py
+++ lessons/lesson_25/homework_25_xpath.py
@@ -1,74 +1,67 @@
+# XPath
 
-#XPath
-
-#Header
-#Home Button
+# Header
+# Home Button
 """//a[text()='Home']"""
-#About Button
+# About Button
 """//button[text()='About']"""
-#Contacts Button
+# Contacts Button
 """//button[text()='Contacts']"""
-#Guest log in Button
+# Guest log in Button
 """//button[text()='Guest log in']"""
-#Sighn In Button
+# Sign In Button
 """//button[text()='Sign In']"""
 
-
-
-#Do More text
+# Do More text
 """//h1[text()='Do more!']"""
-#or
+# or
 """//div[@class='hero-descriptor']/h1[1]"""
 
-#With the help of the Hillel ... text
+# With the help of the Hillel ... text
 """//div[@class='hero-descriptor']/p[1]"""
 
-#Sign Up Buton
+# Sign Up Buton
 """//div[@class='hero-descriptor']/button[1]"""
-#or
+# or
 """//button[text()='Sign up']"""
 
-#Player with video
+# Player with video
 """//div[@class='player-controls-content']"""
 
-#Fuel expenses image
+# Fuel expenses image
 """(//div[@class='about-block'])[1]/div/img"""
-#or
+# or
 """(//img[@class='about-picture_img'])[1]"""
 
-
-#Log fuel expenses text
+# Log fuel expenses text
 """(//div[@class='about-block'])[1]/p[1]"""
 
-#Keep track of your ... text
+# Keep track of your ... text
 """(//div[@class='about-block'])[1]/p[2]"""
-#or
+# or
 """(//p[@class='about-block_descr lead'])[1]"""
 
-#Instructions image
+# Instructions image
 """(//div[@class='about-block'])[2]/div/img"""
-#or
+# or
 """(//img[@class='about-picture_img'])[2]"""
 
-#Instructions and manuals text
+# Instructions and manuals text
 """(//div[@class='about-block'])[2]/p[1]"""
 
-#Watch over 100 ... text
+# Watch over 100 ... text
 """(//div[@class='about-block'])[2]/p[2]"""
-#or
+# or
 """(//p[@class='about-block_descr lead'])[2]"""
 
-#Telegram icon
+# Telegram icon
 """//span[@class='socials_icon icon icon-telegram']"""
 
-#linl to Hillel page
+# linl to Hillel page
 """//a[@href='https://ithillel.ua']"""
 
-#© 2021 Hillel IT school text
+# © 2021 Hillel IT school text
 """//p[text()='© 2021 Hillel IT school']"""
 
-#link to support email
+# link to support email
 """//a[@class='contacts_link h4']"""
-
-
-
---
+++ lessons/lesson_25/homework__25_css.py
@@ -0,0 +1,30 @@
+# CSS
+
+# Header
+# Home Button
+"""a.btn"""
+# About Button
+"""button[appscrollto="aboutSection"]"""
+# Contacts Button
+"""button[appscrollto="contactsSection"]"""
+# Guest log in Button
+"""button.header-link.-guest"""
+# Sign In Button
+"""button.btn.btn-outline-white.header_signin"""
+
+# Do More text
+"""h1.hero-descriptor_title.display-2"""
+# With the help of the Hillel ... text
+"""p.hero-descriptor_descr.lead"""
+# Sign Up Buton
+"""button.hero-descriptor_btn.btn.btn-primary"""
+# Player with video
+"""div#player-controls"""
+# Fuel expenses image
+"""img[src="/assets/images/homepage/info_1.jpg"]"""
+# Log fuel expenses text
+
+# Keep track of your ... text
+
+# Instructions image
+"""img[src="/assets/images/homepage/info_2.jpg"]"""
