cc0383bcfc7b55185f340fbda2259de0cee25c59
nedzte07-eng <nedzte07@gmail.com>
2026-03-26 11:23:21 +0200

    CSS locators finish

--- lessons/lesson_25/homework__25_css.py
+++ lessons/lesson_25/homework__25_css.py
@@ -1,6 +1,8 @@
 # CSS
 
 # Header
+#Hillel Auto logo
+"""svg[xmlns="http://www.w3.org/2000/svg"][width="135"]"""
 # Home Button
 """a.btn"""
 # About Button
@@ -23,8 +25,33 @@
 # Fuel expenses image
 """img[src="/assets/images/homepage/info_1.jpg"]"""
 # Log fuel expenses text
-
+"""p.about-block_title.h2"""  # not unic
 # Keep track of your ... text
-
+"""p.about-block_descr"""  # not unic
 # Instructions image
 """img[src="/assets/images/homepage/info_2.jpg"]"""
+# Instructions and manuals text
+""".col-12.col-lg-6.mt-lg-0.mt-md-5.mt-sm-4.mt-3 > .about-block > p.about-block_title.h2"""
+# Watch over 100 ... text
+""".col-12.col-lg-6.mt-lg-0.mt-md-5.mt-sm-4.mt-3 > .about-block > p.about-block_descr"""
+# Contacts text
+""".col-md-6.d-flex.flex-column.align-items-center.align-items-md-start > h2"""
+# Facebook icon
+"""span.socials_icon.icon.icon-facebook"""
+# Telegram icon
+"""span.socials_icon.icon.icon-telegram"""
+# Youtube icon
+"""span.socials_icon.icon.icon-youtube"""
+# Instagram icon
+"""span.socials_icon.icon.icon-instagram"""
+# Linkedin icon
+"""span.socials_icon.icon.icon-linkedin"""
+# Link to Hillel site
+"""a[href="https://ithillel.ua"]"""
+# Link to support
+"""a[href="mailto:developer@ithillel.ua"]"""
+# Footer logo
+"""a.footer_logo"""
+# © 2021 Hillel IT school text
+""".col-7.d-flex.flex-column.justify-content-center.footer_item.-left > p""" # not unic
+
