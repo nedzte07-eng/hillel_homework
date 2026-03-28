078a21032d54819ce8a534c9c96426182f370f25
nedzte07-eng <nedzte07@gmail.com>
2026-03-28 17:45:02 +0200

    added show-trace command

--- tests/lesson_26/test_lesson_26.py
+++ tests/lesson_26/test_lesson_26.py
@@ -4,7 +4,9 @@ import os
 BASE_URL = os.getenv("RC1_ORION")
 
 # to run in bash
-# python -m pytest --headed --browser=chromium
+# python -m pytest --headed --browser=chromium --tracing on
+#trace view
+# python -m playwright show-trace test-results/tests-lesson-26-test-lesson-26-py-test-check-login-chromium/trace.zip
 
 
 def test_check_login(ui_login):
