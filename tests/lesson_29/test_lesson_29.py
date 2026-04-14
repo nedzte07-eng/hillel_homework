855decbbb47e519973ffe1f48280f48c34159b38
nedzte07-eng <nedzte07@gmail.com>
2026-04-14 09:46:22 +0300

    Homework 29 added db works with internal db, deleted comments

--- tests/lesson_29/test_lesson_29.py
+++ tests/lesson_29/test_lesson_29.py
@@ -1,8 +1,10 @@
 import psycopg2
 
+
 def test_database_connection(db_conn):
     conn = db_conn
     assert conn is not None
+
 
 def test_insert_data(cursor_con):
     cursor, db_conn = cursor_con
@@ -12,6 +14,7 @@ def test_insert_data(cursor_con):
     result = cursor.fetchone()
     assert result[1] == 'Pasha'
 
+
 def test_edit_data(cursor_con):
     cursor, db_conn = cursor_con
     cursor.execute("UPDATE public.lesson29 SET city = 'Mukachevo' WHERE name = 'Pasha'")
@@ -20,6 +23,7 @@ def test_edit_data(cursor_con):
     result = cursor.fetchone()
     assert result[0] == 'Mukachevo'
 
+
 def test_delete_data(cursor_con):
     cursor, db_conn = cursor_con
     cursor.execute("DELETE FROM public.lesson29 WHERE name = 'Pasha'")
@@ -27,29 +31,3 @@ def test_delete_data(cursor_con):
     cursor.execute("SELECT * FROM public.lesson29 WHERE name = 'Pasha'")
     result = cursor.fetchone()
     assert result is None
-
-# def test_database_connection():
-#     conn = psycopg2.connect(
-#         dbname="postgres",
-#         user="postgres",
-#         password="569300",
-#         host="db",  # ім'я сервісу з docker-compose
-#         port=5432
-#     )
-#     assert conn is not None
-#
-# def test_insert_data():
-#     conn = psycopg2.connect(
-#         dbname="postgres",
-#         user="postgres",
-#         password="569300",
-#         host="host.docker.internal",  # ім'я сервісу з docker-compose
-#         port=5432
-#     )
-#     cursor = conn.cursor()
-#     cursor.execute("INSERT INTO public.lesson29 (id, name, city, phone) VALUES (4, 'Pasha', 'Jitomir', '555-66-77')")
-#     conn.commit()
-#     cursor.execute("SELECT * FROM public.lesson29 WHERE phone = '555-66-77'")
-#     result = cursor.fetchone()
-#     assert result[1] == 'Pasha'
-
