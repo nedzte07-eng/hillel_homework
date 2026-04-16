720799cd1e2b794e4808ba6ac7ea7ee45ef49629
nedzte07-eng <nedzte07@gmail.com>
2026-04-16 12:37:41 +0300

    some changes

--- tests/api_tests/lesson_13_tests/test_homework_13.py
+++ tests/api_tests/lesson_13_tests/test_homework_13.py
@@ -9,13 +9,17 @@ console_handler.setFormatter(log_format)
 _log.addHandler(console_handler)
 _log.setLevel(logging.DEBUG)
 
+
 class TestContent:
 
+
     @pytest.mark.external_api
+
     def test_signin_with_existence_user_200(self, sign_up):
         user_id = sign_up.json()["data"]["userId"]
         assert sign_up.status_code == 200, "Response status code is incorrect"
         assert user_id == 329784, "User was not signed in"
+
 
     @pytest.mark.external_api
     def test_adding_a_car_201(self, sign_up, base_url, auth, car_credential):
@@ -38,13 +42,25 @@ class TestContent:
         assert data_car["logo"] == "bmw.png"
 
 
-
     @pytest.mark.external_api
     def test_if_car_is_present_200(self, car_data):
         data_car = car_data.json()["data"][0]
         assert car_data.status_code == 200, "Response status code is incorrect"
         assert data_car["id"] is not None, "Id was not found"
         assert data_car["carBrandId"] == 2
+        assert data_car["carModelId"] == 8
+        assert data_car["initialMileage"] == 555
+        assert data_car["brand"] == "BMW"
+        assert data_car["model"] == "X5"
+        assert data_car["logo"] == "bmw.png"
+
+
+    @pytest.mark.external_api
+    def test_if_car_is_present_200_failed_intentionally(self, car_data):
+        data_car = car_data.json()["data"][0]
+        assert car_data.status_code == 200, "Response status code is incorrect"
+        assert data_car["id"] is not None, "Id was not found"
+        assert data_car["carBrandId"] == 4
         assert data_car["carModelId"] == 8
         assert data_car["initialMileage"] == 555
         assert data_car["brand"] == "BMW"
@@ -63,6 +79,7 @@ class TestContent:
         assert response_car.json()["status"] == "ok", "Car was not deleted"
         assert deleted_car_id == car_id, "Seems was deleted wrong car"
 
+
     @pytest.mark.external_api
     def test_if_car_is_absent_200(self, sign_up, base_url):
         token_session = sign_up.cookies.get("sid")
@@ -71,6 +88,7 @@ class TestContent:
         data = response_car.json()["data"]
         assert response_car.status_code == 200, "Response status code is incorrect"
         assert data == [], "Cars were found"
+
 
     @pytest.mark.external_api
     def test_user_logout_200(self, sign_up, base_url):
